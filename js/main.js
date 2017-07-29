07c45ded0ee10f5be3edf4e9794579866341a29e
Koyta <koytaboyta@gmail.com>
2017-07-29 15:01:42 +0300

    Add horizontal scroll via navigation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
 $(document).ready(function () {
+  'use strict';
+  //Sidebar reveal
+  $(".asideTrigger").click(function () {
+    $("body").toggleClass('showAside');
+  });
 
-    //Sidebar reveal
-    $(".asideTrigger").click(function () {
-        $("body").toggleClass('showAside');
-    })
+    $('header nav a').on('click', function() {
+
+        console.log(`===START===`);
+
+        const anchor_view = $(this).attr('href');
+        console.log(`Anchor: ${anchor_view}`);
+
+        const data_view = anchor_view.substring(1);
+        console.log(`Data view: ${data_view}`);
+
+        const currentOffset = $(window).scrollLeft();
+        console.log(`Current offset: ${currentOffset}`);
+
+        // TODO: Нужно сделать скролл до того div'а, на который сослался клик, тобишь до anchor_view/data_view
+
+        const element = $(" section[data-view='"+data_view+"'] ")
+
+        // Получаем отступ слева у этого элемента
+        const element_offset = element.offset().left;
+        console.log(`Element offset: ${element_offset}`);
+
+        // Скроллим
+        $('html').animate({
+            scrollLeft: element_offset
+        }, 800, 'easeInOutCubic');
+
+        console.log(`===END===`);
+    });
+
 });
