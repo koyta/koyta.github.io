7afb604933f9dcddf29c0ccb20c2e440493b14a3
Koyta <koytaboyta@gmail.com>
2017-08-03 00:46:12 +0300

    sidebar toggle fix, warning for unsupported display:grid browsers

--- js/main.js
+++ js/main.js
@@ -1,39 +1,52 @@
 $(document).ready(function () {
-  'use strict';
-  //Sidebar reveal
-  $(".asideTrigger").click(function () {
-    //TODO Make it with VueJS
-    $("body").toggleClass('showAside');
-    $(this).toggleClass('current');
-  });
 
-    $('header nav a').on('click', function() {
+    // Warning
+    const warningSection = $(".warning");
+    if (Modernizr.cssgrid) {
+        warningSection.show();
+        console.log("Warning showed");
+        $(".grid-container").css('display', 'none');
+    } else {
+        warningSection.hide();
+        console.log("Warning hidden");
+    }
 
-        console.log(`===START===`);
+    // Save to the variable current window offset
+    const currentOffset = $(window).scrollLeft();
+
+    // Sidebar reveal
+    let isSidebarRevealed = false;
+    // Trigger on click show/hide sidebar
+    $(".asideTrigger").click(function () {
+        // Toggle sidebar open status
+        $("body").toggleClass('showAside');
+        $(this).toggleClass('current');
+    });
+
+    $('header nav a').on('click', function () {
+
+        // console.log(`===START===`);
 
         const anchor_view = $(this).attr('href');
-        console.log(`Anchor: ${anchor_view}`);
+        // console.log(`Anchor: ${anchor_view}`);
 
         const data_view = anchor_view.substring(1);
-        console.log(`Data view: ${data_view}`);
-
-        const currentOffset = $(window).scrollLeft();
-        console.log(`Current offset: ${currentOffset}`);
+        // console.log(`Data view: ${data_view}`);
 
         // TODO: Нужно сделать скролл до того div'а, на который сослался клик, тобишь до anchor_view/data_view
 
-        const element = $(" section[data-view='"+data_view+"'] ")
+        const element = $(" section[data-view='" + data_view + "'] ")
 
         // Получаем отступ слева у этого элемента
         const element_offset = element.offset().left;
-        console.log(`Element offset: ${element_offset}`);
+        // console.log(`Element offset: ${element_offset}`);
 
-        // Скроллим
+        // Scroll
         $('html,body').animate({
             scrollLeft: element_offset
-        }, 800, 'easeInOutCubic');
+        }, 600, 'easeInOutCubic');
 
-        console.log(`===END===`);
+        // console.log(`===END===`);
     });
 
 });
